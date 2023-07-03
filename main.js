55f9eea6539d93d42e22062743f0b74e5699d9f8
Khurmatbek <xurmatbekolloyorov1514@gmail.com>
2023-07-03 20:05:12 +0500

    add new class

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 var ElBtn = document.querySelector(".header__btn");
-ElBtn.addEventListener("click", function () {
-    document.body.classList.toggle("open");
+ElBtn.addEventListener("click", function ()
+ {
+    
+    if(document.body.style.width < "575px"){
+       document.body.classList.toggle ("open");
+    }
+    else
+    {
+        document.body.classList.remove("open");
+    }
 }
-)
+)   
 
